20707a3ef7983fd49ca916fc1bbc2ba8110b30c8
Tomek O <34099068+tomeko74@users.noreply.github.com>
2019-11-23 17:42:46 +0100

    326 Session Sign In

--- 13 Sessions and JWT/smart-brain/src/App.js
+++ 13 Sessions and JWT/smart-brain/src/App.js
@@ -60,7 +60,20 @@ class App extends Component {
         .then(resp => resp.json())
         .then(data => {
           if (data && data.id) {
-            console.log('success we need to get a user profile')
+            fetch(`http://localhost:3000/profile/${data.id}`, {
+              method: 'get',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+              }      
+            })
+            .then(resp => resp.json())
+            .then(user => {
+              if(user && user.email) {
+                this.loadUser(user)
+                this.onRouteChange('home');
+              }
+            })
           }
         })
         .catch(console.log)
