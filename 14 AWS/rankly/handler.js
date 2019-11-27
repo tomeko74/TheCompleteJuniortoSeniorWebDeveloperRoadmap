2abf2ebd20d0390944f846a9fc703c113bbe7735
Tomek O <34099068+tomeko74@users.noreply.github.com>
2019-11-27 15:38:41 +0100

    346 Rank in AWS lambda

---
+++ 14 AWS/rankly/handler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const emojis = [
+  '😄','😃','😀','😊','😉','😍','🔶','🔷', '🚀'
+];
+
+module.exports.rank = async event => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+    },
+    body: JSON.stringify(
+      {
+        message: 'Rank generated!',
+        input: rankEmoji,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
