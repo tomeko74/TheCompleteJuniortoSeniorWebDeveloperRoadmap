fbc86ca676dbd349f3188d2028810b2e98a4eaf2
Tomek O <34099068+tomeko74@users.noreply.github.com>
2019-11-28 21:46:52 +0100

    AWS lambda complete

--- 14 AWS/rankly/handler.js
+++ 14 AWS/rankly/handler.js
@@ -6,12 +6,12 @@ const emojis = [
 
 module.exports.rank = async event => {
   const rank = event.queryStringParameters.rank;
-  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
-  
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+      'Access-Control-Allow-Origin' : '*' // Required for CORS support to work
     },
     body: JSON.stringify(
       {
